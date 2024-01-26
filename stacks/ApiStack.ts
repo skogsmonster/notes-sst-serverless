5c68857cc502f3a39ac7da6fa5286c3640a9f092
Reed Anderson <reed.forestmonster@gmail.com>
2024-01-25 16:01:13 -0800

    Add PUT

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -15,6 +15,7 @@ export function ApiStack({ stack }: StackContext) {
       "POST /notes": "packages/functions/src/create.main",
       "GET /notes/{id}": "packages/functions/src/get.main",
       "GET /notes": "packages/functions/src/list.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
     },
   });
 
