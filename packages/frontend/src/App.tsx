fb34a4f3b3a0eb552814724fab66a7475efe1677
Reed Anderson <reed.forestmonster@gmail.com>
2024-02-01 13:56:35 -0800

    Add logout

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
 
 import Routes from "./Routes.tsx";
-
+import { AppContext, AppContextType } from "./lib/contextLib";
 import "./App.css";
 
 function App() {
+
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
@@ -15,17 +23,25 @@ function App() {
         </LinkContainer>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Nav activeKey={window.location.pathname}>
-            <LinkContainer to="/signup">
-              <Nav.Link>Signup</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
-          </Nav>
+          {isAuthenticated ? (
+            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+          ) : (
+            <>
+              <LinkContainer to="/signup">
+                <Nav.Link>Signup</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            </>
+          )}
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated } as AppContextType}
+      >
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -6,7 +6,10 @@ import Stack from "react-bootstrap/Stack";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
 
+import { useAppContext } from "../lib/contextLib";
+
 export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -19,7 +22,7 @@ export default function Login() {
 
       try {
           await Auth.signIn(email, password);
-          alert("Logged in");
+          userHasAuthenticated(true);
       } catch (error) {
           // Prints the full error
           console.error(error);
